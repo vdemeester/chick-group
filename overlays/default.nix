aea856640a107c09a3b9be4f4af445f0108cc0b5
Vincent Demeester <vincent@sbr.pm>
2023-12-01 16:24:44 +0100

    Add kss at 0.2.1
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,7 @@
 self: super:
 {
   inherit (super.callPackage ../packages/deptree.nix { }) deptree;
+  inherit (super.callPackage ../packages/kss.nix { }) kss;
   inherit (super.callPackage ../packages/snazy.nix { }) snazy;
   inherit (super.callPackage ../packages/tkn-pac.nix { })
     tkn-pac
