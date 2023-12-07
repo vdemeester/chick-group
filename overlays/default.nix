d015187920ebc23b592a2e9ba8a4484ff7119888
Vincent Demeester <vincent@sbr.pm>
2023-12-07 13:32:38 +0100

    Remove snazy, it is available from nixpkgs
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,6 @@ self: super:
 {
   inherit (super.callPackage ../packages/deptree.nix { }) deptree;
   inherit (super.callPackage ../packages/kss.nix { }) kss;
-  inherit (super.callPackage ../packages/snazy.nix { }) snazy;
   inherit (super.callPackage ../packages/tkn-pac.nix { })
     tkn-pac
     tkn-pac_0_22
