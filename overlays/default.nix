68d5014469943487d78c760bef9c9258a80c8749
Vincent Demeester <vincent@sbr.pm>
2024-05-17 10:43:52 +0200

    tkn-pac: update 0.24 and add 0.26 and 0.27
    Closes #55
    Closes #54
    Closes #49
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,6 +4,8 @@ self: super:
   inherit (super.callPackage ../packages/kss.nix { }) kss;
   inherit (super.callPackage ../packages/tkn-pac.nix { })
     tkn-pac
+    tkn-pac_0_27
+    tkn-pac_0_26
     tkn-pac_0_25
     tkn-pac_0_24
     tkn-pac_0_23
