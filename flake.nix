c099ce3423fc60323816aba78eaaa80223bb891c
Vincent Demeester <vincent@sbr.pm>
2024-06-13 17:38:55 +0200

    flake: add darwin platform back…
    … and test part of them with github actions
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       flake = {
         githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
-          checks = nixpkgs.lib.getAttrs [ "x86_64-linux" ] self.packages;
+          checks = nixpkgs.lib.getAttrs [ "x86_64-linux" "x86_64-darwin" ] self.packages;
         };
         overlays = {
           default = final: prev: import ./overlays final prev;
