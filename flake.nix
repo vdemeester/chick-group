dd8105297022f7ad6e57d511518561c99abfb46a
Vincent Demeester <vincent@sbr.pm>
2024-06-13 18:39:53 +0200

    Add pre-commit-hook and configure it
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-parts, pre-commit-hooks, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       flake = {
         githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
@@ -26,11 +26,29 @@
               drvAttrs = builtins.filter (n: lib.isDerivation pkgs.${n}) overlayAttrs;
             in
             lib.listToAttrs (map (n: lib.nameValuePair n pkgs.${n}) drvAttrs);
+          checks = {
+            pre-commit-check = pre-commit-hooks.lib.${system}.run {
+              src = ./.;
+              hooks = {
+                nixpkgs-fmt.enable = true;
+                # nix-linter.enable = true;
+                # statix.enable = true;
+              };
+            };
+          };
+          devShells.default = pkgs.mkShell {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+            buildInputs = with pkgs; [
+              pre-commit
+            ];
+          };
         };
     };
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
   inputs.flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  inputs.pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nix-github-actions.url = "github:nix-community/nix-github-actions";
   inputs.nix-github-actions.inputs.nixpkgs.follows = "nixpkgs";
 }
