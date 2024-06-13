78de3687e2dba5a4848b033b7ba3507798d551d6
Vincent Demeester <vincent@sbr.pm>
2024-06-13 18:41:49 +0200

    Formiting everything
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- flake.nix
+++ flake.nix
@@ -44,6 +44,7 @@
           };
         };
     };
+
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
   inputs.flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
