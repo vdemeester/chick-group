89d0788609947dc58968252b155f1d81ce0dd6a0
Vincent Demeester <vincent@sbr.pm>
2025-10-02 15:49:31 +0200

    Remove tkn-pac, it's now upstream.

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,13 +1,4 @@
-self: super:
-{
+self: super: {
   inherit (super.callPackage ../packages/deptree.nix { }) deptree;
   inherit (super.callPackage ../packages/kss.nix { }) kss;
-  inherit (super.callPackage ../packages/tkn-pac.nix { })
-    tkn-pac
-    tkn-pac_0_28
-    tkn-pac_0_27
-    tkn-pac_0_26
-    tkn-pac_0_25
-    tkn-pac_0_24
-    ;
 }
